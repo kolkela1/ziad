02f8abdc25fbeaad333c0e9b8471f7c44c71f46b
kolkela1 <kolkala01090765646@gmail.com>
2025-10-08 17:38:34 +0300

    Delete product.js

--- product.js
+++
@@ -1,9 +0,0 @@
-const toggleBtn = document.getElementById("menu-toggle");
-const navLinks = document.getElementById("nav-links");
-const navButtons = document.getElementById("nav-buttons");
-
-toggleBtn.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
-  navButtons.classList.toggle("show");
-});
-
